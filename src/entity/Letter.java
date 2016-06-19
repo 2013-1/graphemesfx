d3da58120ff89c3b532c436c274e2af9bc5820d6
Yan Kaic <Yan Kaic@Pindaiba>
2016-06-19 09:30:11 -0300

    Inserindo uma letra na SwipeView

--- src/entity/Letter.java
+++ src/entity/Letter.java
@@ -5,21 +5,11 @@
  */
 package entity;
 
-import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseMotionListener;
-import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 
 /**
  *
@@ -30,72 +20,47 @@ public class Letter extends ImageView {
   private Point oldLocation;
   private boolean lowercase;
   private String name;
-  
+
   public Letter() {
     this("A");
   }
 
   public Letter(String name) {
-    super();    
-    this.name = name;  
+    super();
+    this.name = name;
     init();
   }
-  
-  
 
   private void init() {
-    try {
-      String letter = getName().toLowerCase();
-      
-      lowercase = letter.equals(getName());
-      URL url = new URL(((lowercase) ? getLowerCasePath() : getUpperCasePath()) 
-              + "letter.png");
-      Image image = new Image(url.toString());
-      
-      setImage(image);
-      ImageIcon icon = new ImageIcon(url);
-      setFitWidth(image.getWidth());
-      setFitHeight(image.getHeight());
-      
-      
-      addMouseMotionListener(new MouseMotionAdapter() {
-        @Override
-        public void mouseDragged(MouseEvent e) {
-          Point mouse = MouseInfo.getPointerInfo().getLocation();
-//          Point tela = getLocationOnScreen();
-//
-//          tela.x -= getLocation().x;
-//          tela.y -= getLocation().y;
-//
-//          mouse.x -= tela.x + getWidth() / 2;
-//          mouse.y -= tela.y + getHeight() / 2;
-//          setLocation(mouse);
-        }
-      });
-    }
-    catch (MalformedURLException ex) {
-      Logger.getLogger(Letter.class.getName()).log(Level.SEVERE, null, ex);
-    }
-  }
 
-  public void setOldLocation(Point location) {
-    this.oldLocation = location;
-  }
+    String letter = getName().toLowerCase();
 
-  public Point getOldLocation() {
-    return oldLocation;
+    lowercase = letter.equals(getName());
+    String url = (lowercase) ? getLowerCasePath() : getUpperCasePath();
+    url += "letter.png";
+    Image image = new Image(url);
+
+    setImage(image);
+    setFitWidth(image.getWidth());
+    setFitHeight(image.getHeight());
+
+    setOnMouseDragged((javafx.scene.input.MouseEvent event) -> {
+      setX(event.getX() - getFitWidth() / 2);
+      setY(event.getY() - getFitHeight() / 2);
+    });
+
   }
 
   public String getLowerCasePath() {
     char bar = '/';
-    return getClass().getResource(bar + "letters" + bar
+    return getClass().getResource(bar + "resource" + bar
             + getName().trim().toLowerCase() + bar
             + "lowercase" + bar).toString();
   }
 
   public String getUpperCasePath() {
     char bar = '/';
-    String path = bar + "letters" + bar
+    String path = bar + "resource" + bar
             + getName().trim().toLowerCase() + bar
             + "uppercase" + bar;
     URL resource = getClass().getResource(path);
@@ -113,7 +78,5 @@ public class Letter extends ImageView {
   public void setName(String name) {
     this.name = name;
   }
-  
-  
 
 }
--- src/gui/SwipeViewController.java
+++ src/gui/SwipeViewController.java
@@ -5,13 +5,17 @@
  */
 package gui;
 
+import entity.Letter;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.TranslateTransition;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 /**
@@ -22,21 +26,46 @@ import javafx.util.Duration;
 public class SwipeViewController implements Initializable {
 
   @FXML
-  private ImageView oi;
-  
+  private Pane letterPane;
+  @FXML
+  private ImageView letter;
+  @FXML
+  private AnchorPane draggerPane;
 
   /**
    * Initializes the controller class.
    */
   @Override
   public void initialize(URL url, ResourceBundle rb) {
-    ImageView letter = new ImageView();
-    oi.setLayoutX(900);
-    TranslateTransition transition = new TranslateTransition(new Duration(1000), oi);
-    transition.setToX(-900);
-    transition.play();
+//    ImageView letter = new ImageView();
+//    oi.setLayoutX(900);
+//    TranslateTransition transition = new TranslateTransition(new Duration(1000), oi);
+//    transition.setToX(-900);
+//    transition.play();
+    Letter letter = new Letter();
+    letter.setX(900);
+    letterPane.getChildren().add(letter);
+    draggerPane.setVisible(false);
+    letterEvents(letter);
     
+    TranslateTransition translate = new TranslateTransition(new Duration(2000), letter);
+    translate.setToX(-900);
+    translate.play();
+    
+
     // TODO
-  }  
+  }
   
+  private void letterEvents(Letter letter){
+    letter.setOnMousePressed((MouseEvent event) -> {
+      if (draggerPane.getChildren().isEmpty()) {
+        letterPane.getChildren().remove(letter);
+        draggerPane.getChildren().add(letter);
+        draggerPane.setVisible(true);
+        letter.setX(letter.getX() + letterPane.getLayoutX());
+        letter.setY(letter.getY() + letterPane.getLayoutY());
+      }
+    });
+  }
+
 }
